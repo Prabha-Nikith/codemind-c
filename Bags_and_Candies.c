4e18bb913fac7b5bc633a28193a3ed59f4039392
Prabha Nikith <119718378+Prabha-Nikith@users.noreply.github.com>
2022-12-10 10:38:02 +0530

    #include<stdio.h> int main() {     int a,b,c,d; 	scanf("%d%d%d",&a,&b,&c); 	d=b*c; 	if(a%d==0) 	{ 	printf("%d",a/d); 	} 	else if(a%d!=0) 	{ 	printf("%d",(a/d)+1); 	} }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+	scanf("%d%d%d",&a,&b,&c);
+	d=b*c;
+	if(a%d==0)
+	{
+	printf("%d",a/d);
+	}
+	else if(a%d!=0)
+	{
+	printf("%d",(a/d)+1);
+	}
+}
