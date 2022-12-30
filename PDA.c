a67b1d611baa23b23ca3db88042acef4c481a802
Prabha Nikith <119718378+Prabha-Nikith@users.noreply.github.com>
2022-12-30 10:11:17 +0530

    #include<stdio.h> int main() {     int a,i,d=0;     scanf("%d",&a);     for(i=1;i<a;i++)     {         if(a%i==0)         {             d+=i;         }     }     if(d>a) printf("ABUNDANT");     else if(d==a) printf("PERFECT");     else if(d<a) printf("DEFICIENT"); }

---
+++ PDA.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,d=0;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            d+=i;
+        }
+    }
+    if(d>a) printf("ABUNDANT");
+    else if(d==a) printf("PERFECT");
+    else if(d<a) printf("DEFICIENT");
+}
