8507098fa301e33f657ff00c77180c5c96580ec0
Prabha Nikith <119718378+Prabha-Nikith@users.noreply.github.com>
2023-01-20 12:01:10 +0530

    #include<stdio.h> int main() {     int a,b,n;     scanf("%d",&n);     for(a=1;a<=n;a++)     {         for(b=1;b<=n;b++)         printf("%d ",a);         printf(" ");     } }

---
+++ Pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,n;
+    scanf("%d",&n);
+    for(a=1;a<=n;a++)
+    {
+        for(b=1;b<=n;b++)
+        printf("%d ",a);
+        printf("
+");
+    }
+}    
+
